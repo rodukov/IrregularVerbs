f119f782fa41f5205cd204ffac97690fdfc1943f
Gleb Rodukov <electroneuphoria@gmail.com>
2025-01-01 20:05:09 +0300

    remove

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', name: 'home', component: () => import('../views/HomeView.vue') }
+  { path: '/archived', name: 'home', component: () => import('../views/HomeView.vue') }
 
 ]
 
